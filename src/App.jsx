bb16fb5c1889fd0e9fb3c87ca63b6535d4b0eb83
Nafisamusani53 <nafisamusani53@gmail.com>
2025-05-03 13:00:05 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='w-full h-full bg-black text-white'>
+      bye
+    </div>
+  )
+}
+
+export default App
