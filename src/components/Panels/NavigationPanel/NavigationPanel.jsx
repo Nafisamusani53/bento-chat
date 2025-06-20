52f471764b6bec12ac6cdc48ec6e1592fe8c21bf
Nafisamusani53 <nafisamusani53@gmail.com>
2025-06-20 13:17:39 +0530

    404 error solving

--- src/components/Panels/NavigationPanel/NavigationPanel.jsx
+++ src/components/Panels/NavigationPanel/NavigationPanel.jsx
@@ -4,48 +4,51 @@ import { ChatIcon, SettingsIcon } from '../../icons'
 import { setNavigation } from '../../../reducers/navigationSlice'
 
 function NavigationPanel() {
-    const profile = useSelector((state)=>state.profile)
-    const selected = useSelector((state)=>state.navigation)
+    const profile = useSelector((state) => state.profile)
+    const selected = useSelector((state) => state.navigation)
     const dispatch = useDispatch()
     const navigationHandler = (e, tab) => {
         e.preventDefault()
         dispatch(setNavigation(tab))
     }
-  return (
-    <div className = 'flex flex-col justify-between items-center !px-3 !py-8 h-full rounded-2xl bg-white/50 border-white/50 border'>
-        {/* upper navigation panel */}
-        <div className='flex flex-col gap-4 items-center justify-center'>
+    return (
+        <div className='flex flex-col justify-between items-center !px-3 !py-8 h-full rounded-2xl bg-white/50 border-white/50 border'>
+            {/* upper navigation panel */}
+            <div className='flex flex-col gap-4 items-center justify-center'>
 
-            <div className={`flex items-center justify-center w-full rounded-[10px] !px-2 ${selected.search && ('!py-3 bg-white/40')}`}
-                onClick={(e)=>{navigationHandler(e,"search")}}>
-                <ChatIcon/>
+                <div className={`flex items-center justify-center w-full rounded-[10px] !px-2 cursor-pointer transition-all duration-300 ease-in-out
+                                ${selected.search ? '!py-3 bg-white/40' : 'bg-transparent'}`}
+                    onClick={(e) => { navigationHandler(e, "search") }}>
+                    <ChatIcon />
+                </div>
+
+                {/* other icons in future */}
+
+
             </div>
 
-            {/* other icons in future */}
+            {/* lower navigation panel */}
+            <div className='flex flex-col gap-4 items-center justify-center'>
 
+                <div className={`flex items-center justify-center w-full rounded-[10px] !px-2 cursor-pointer transition-all duration-300 ease-in-out
+                                ${selected.settings ? '!py-3 bg-white/40' : 'bg-transparent'}`}
+                    onClick={(e) => { navigationHandler(e, "settings") }}>
+                    <SettingsIcon />
+                </div>
 
-        </div>
-
-        {/* lower navigation panel */}
-        <div className='flex flex-col gap-4 items-center justify-center'>
-
-            <div className={`flex items-center justify-center w-full rounded-[10px] !px-2  ${selected.settings && ('!py-3 bg-white/40')}`}
-                onClick={(e)=>{navigationHandler(e,"settings")}}>
-                <SettingsIcon/>
+                <div className={`flex items-center justify-center w-full rounded-[10px] !px-2 cursor-pointer transition-all duration-300 ease-in-out
+                                ${selected.profile ? '!py-3 bg-white/40' : ' bg-transparent'}`}
+                    onClick={(e) => { navigationHandler(e, "profile") }}>
+                    <img
+                        src={profile.avatar}
+                        className='border border-dark-blue rounded-full aspect-square w-9 object-cover'
+                        alt='Profile image'
+                        loading='lazy'
+                    />
+                </div>
             </div>
-
-              <div className={`flex items-center justify-center w-full rounded-[10px] !px-2  ${selected.profile ? '!py-3 bg-white/40' : ''}`}
-                  onClick={(e) => { navigationHandler(e, "profile") }}>
-                      <img
-                          src={profile.avatar}
-                          className='border border-dark-blue rounded-full aspect-square w-9 object-cover'
-                          alt='Profile image'
-                          loading='lazy'
-                      />
-              </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default NavigationPanel
