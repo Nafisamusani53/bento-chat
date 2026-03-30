3b158608462415edb92249baa2ccf7174923ad5c
Nafisamusani53 <nafisamusani53@gmail.com>
2026-03-30 18:26:01 +0530

    added google auth service

--- components/Auth/ThirdParty.tsx
+++ components/Auth/ThirdParty.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import AuthButton from './AuthButton'
-import { AppleIcon, GoogleIcon, RightArrow } from '../icons'
+import { GoogleIcon, RightArrow } from '../icons'
+import { googleAuth } from '@/store/thunks/AuthThunks'
 
 const ThirdParty = () => {
   return (
     <div className="flex flex-col gap-4 w-full items-center justify-between">
-      <AuthButton AuthIcon={GoogleIcon} text={'Continue with Google'} EndIcon={RightArrow} />
+      <AuthButton AuthIcon={GoogleIcon} text={'Continue with Google'} EndIcon={RightArrow} authService={googleAuth}/>
     </div>
   )
 }
